0641d0c05c9b0b4d75b44f00aa38d4a9cf2d5322
thordiichuk <hordiichuk.off@gmail.com>
2025-09-09 23:39:21 +0200

    Add files via upload

---
+++ hord_zewnetrzny.js
@@ -0,0 +1,8 @@
+document.write("Mój numer w dzienniku to 7"+"<br>");
+document.write("Pierwszy wiersz to:"+"&nbsp;&nbsp;10"+"&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;&nbsp;Nie"+"&nbsp;&nbsp;5"+"<br>");
+document.write("Drugi wiersz to:"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;&nbsp;Tak"+"&nbsp;&nbsp;5"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#00FF00").bold().fontsize(5)+"<br>"); 
+document.write(document.lastModified.bold().fontcolor("#808000").strike().fontsize(5));
+
+function okno_zamknij(){ 
+                  window.close() } 
